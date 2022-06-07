62a1b1d7f125b58a80cb90c7454993ce719cc574
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-07 21:36:13 +0200

    Update TestBase.java

--- src/main/java/TestBase.java
+++ src/main/java/TestBase.java
@@ -1,2 +1,26 @@
-public class TestBase {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+
+public class TestBase extends AbstractTestNGCucumberTests {
+    private static WebDriver webDriver;
+
+    @BeforeMethod
+    public static void setUpDriver() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+
+    public static WebDriver getDriver(){
+        return webDriver;
+    }
+
+    @AfterMethod
+    public static void tearDownDriver() {
+        webDriver.quit();
+    }
+
 }
