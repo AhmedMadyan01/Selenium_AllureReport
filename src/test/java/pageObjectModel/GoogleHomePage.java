e5257b2a63848209b9f3d1b318244c7bb765e58b
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-07 21:36:23 +0200

    Update GoogleHomePage.java

--- src/test/java/pageObjectModel/GoogleHomePage.java
+++ src/test/java/pageObjectModel/GoogleHomePage.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import testPackage.TestBase;
 
 public class GoogleHomePage {
     /**
@@ -13,26 +14,21 @@ public class GoogleHomePage {
      */
     private static final By google_logo = By.className("lnXdpd");
 
-
     private static final String google_URL = ("https://www.google.com/");
-
-    private static WebDriver chromeDriver;
 
     /**
      * Methods
      */
     public static void navigateToGoogleHomePage() {
-        WebDriverManager.chromedriver().setup();
-        chromeDriver = new ChromeDriver();
-        chromeDriver.navigate().to(google_URL);
-        chromeDriver.manage().window().maximize();
+        TestBase.getDriver().navigate().to(google_URL);
+        TestBase.getDriver().manage().window().maximize();
     }
 
     public static void validateThatUserRedirectedSuccessfully() {
-        Assert.assertEquals(chromeDriver.getCurrentUrl(), google_URL);
+        Assert.assertEquals(TestBase.getDriver().getCurrentUrl(), google_URL);
     }
 
     public static void validateThatGoogleLogoIconExists() {
-        Assert.assertTrue(chromeDriver.findElement(google_logo).isDisplayed());
+        Assert.assertTrue(TestBase.getDriver().findElement(google_logo).isDisplayed());
     }
 }
