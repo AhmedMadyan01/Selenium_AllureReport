de8448e6e01870cf929ecbf7f336132a224ffb24
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-07 21:14:15 +0200

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -4,7 +4,7 @@ import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(features = {"src/test/java/feature"}
-        , glue = {"tepDefinition"}
+        , glue = {"stepDefinition"}
         , tags = ("@GoogleTest")
 )
 
