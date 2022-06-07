e6a95747af2870c865fd747dcabddea81cd24668
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-07 21:09:18 +0200

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -1,4 +1,12 @@
 package testRunner;
 
-public class TestRunner {
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = {"src/test/java/feature"}
+        , glue = {"tepDefinition"}
+        , tags = ("@GoogleTest")
+)
+
+public class TestRunner  extends AbstractTestNGCucumberTests {
 }
