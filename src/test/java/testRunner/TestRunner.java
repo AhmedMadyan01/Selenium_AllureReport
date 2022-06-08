87ff07f7009ba02b4df413a09ed65588dbfd8f7b
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-08 02:14:50 +0200

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -2,14 +2,29 @@ package testRunner;
 
 import io.cucumber.testng.AbstractTestNGCucumberTests;
 import io.cucumber.testng.CucumberOptions;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import testPackage.CMD;
 import testPackage.TestBase;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
 @CucumberOptions(features = {"src/test/java/feature"}
         , glue = {"stepDefinition"}
         , tags = ("@GoogleTest")
         , plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
-
 )
 
 public class TestRunner extends TestBase {
+    @BeforeSuite
+    public void deleteOutDatedAllureReport() throws IOException {
+        CMD.executeCommandLine("rmdir /Q /S allure-results");
+    }
+
+    @AfterSuite
+    public void generateAllureReport() throws IOException {
+        CMD.executeCommandLine("allure serve allure-results");
+    }
 }
