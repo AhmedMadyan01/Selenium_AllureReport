97602372f01b1d54d0633cac6c1c14799102286b
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-08 12:13:08 +0200

    Update GoogleHomePage_StepDef.java

--- src/test/java/stepDefinition/GoogleHomePage_StepDef.java
+++ src/test/java/stepDefinition/GoogleHomePage_StepDef.java
@@ -1,5 +1,6 @@
 package stepDefinition;
 
+import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
@@ -15,8 +16,24 @@ public class GoogleHomePage_StepDef {
     public void userSuccessfullyNavigated() {
         GoogleHomePage.validateThatUserRedirectedSuccessfully();
     }
+
     @Then("User should see google logo icon displayed")
     public void userShouldSeeGoogleLogoIconDisplayed() {
         GoogleHomePage.validateThatGoogleLogoIconExists();
+    }
+
+    @Then("User should see google search bar displayed")
+    public void userShouldSeeGoogleSearchBarDisplayed() {
+        GoogleHomePage.validateThatGoogleSearchBarExists();
+    }
+
+    @Then("User should see google buttons displayed")
+    public void userShouldSeeGoogleButtonsDisplayed() {
+        GoogleHomePage.validateThatGoogleSearchButtonsDisplayed();
+    }
+
+    @And("User should see google buttons enabled")
+    public void userShouldSeeGoogleButtonsEnabled() {
+        GoogleHomePage.validateThatGoogleSearchButtonClickable();
     }
 }
