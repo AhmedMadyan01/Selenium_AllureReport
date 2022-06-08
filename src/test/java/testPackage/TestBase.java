44092615d77e9c71c6233e91b88eadf695840b62
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-08 02:20:15 +0200

    Update TestBase.java

--- src/test/java/testPackage/TestBase.java
+++ src/test/java/testPackage/TestBase.java
@@ -5,25 +5,37 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
 
 import java.io.IOException;
 
 public class TestBase extends AbstractTestNGCucumberTests {
     private static WebDriver webDriver;
 
-    @BeforeMethod
+    @BeforeMethod(dependsOnMethods = "deleteOutDatedAllureReport")
     public static void setUpDriver() {
         WebDriverManager.chromedriver().setup();
         webDriver = new ChromeDriver();
     }
 
-    public static WebDriver getDriver(){
+    public static WebDriver getDriver() {
         return webDriver;
     }
 
     @AfterMethod
     public static void tearDownDriver() throws IOException {
         webDriver.quit();
+    }
+
+    @AfterSuite
+    public void deleteOutDatedAllureReport() throws IOException {
+        CMD.executeCommandLine("rmdir /Q /S allure-results");
+    }
+
+    @AfterSuite
+    public void generateAllureReport() throws IOException {
+//        CMD.executeCommandLine("allure serve allure-results");
     }
 }
