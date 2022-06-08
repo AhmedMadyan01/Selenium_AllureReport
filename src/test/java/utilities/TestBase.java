0a91409f512201d7a8063a4a35746525d3e4e98b
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-08 03:20:49 +0200

    Create TestBase.java

---
+++ src/test/java/utilities/TestBase.java
@@ -0,0 +1,42 @@
+package utilities;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+
+import java.io.IOException;
+
+public class TestBase extends AbstractTestNGCucumberTests {
+    private static WebDriver webDriver;
+
+    @BeforeMethod
+    public static void setUpDriver() {
+        WebDriverManager.chromedriver().setup();
+        webDriver = new ChromeDriver();
+    }
+
+    public static WebDriver getDriver() {
+        assert webDriver != null;
+        return webDriver;
+    }
+
+    @AfterMethod
+    public static void tearDownDriver() {
+        webDriver.quit();
+    }
+
+    @BeforeSuite
+    public void deleteOutDatedAllureReport() throws IOException {
+        CMD.executeCommandLine("rmdir /Q /S allure-results");
+    }
+
+    @AfterSuite
+    public void generateAllureReport() throws IOException {
+        CMD.executeCommandLine("allure serve allure-results");
+    }
+}
