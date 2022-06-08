4e58affb2cfd7720f30ae8d333e3a92860714ff6
Ahmed Madyan <ahmed.madyan01@gmail.com>
2022-06-08 03:56:28 +0200

    Update TestRunner.java

--- src/test/java/testRunner/TestRunner.java
+++ src/test/java/testRunner/TestRunner.java
@@ -3,7 +3,8 @@ package testRunner;
 import io.cucumber.testng.CucumberOptions;
 import utilities.TestBase;
 
-@CucumberOptions(features = {"src/test/java/feature"}
+@CucumberOptions(publish = true,
+        features = {"src/test/java/feature"}
         , glue = {"stepDefinition"}
         , tags = ("@GoogleTest")
         , plugin = {"pretty", "io.qameta.allure.cucumber7jvm.AllureCucumber7Jvm"}
