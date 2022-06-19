0349e082fe3dd5454a8febd65bad66610c95f4f1
Ahmed Madyan <ahmed.madyan@vodafone.com>
2022-06-20 00:58:58 +0200

    Create ExceptionHandling.java

---
+++ src/main/java/utilities/exception_handling/ExceptionHandling.java
@@ -0,0 +1,9 @@
+package utilities.exception_handling;
+
+public class ExceptionHandling {
+
+    public static void handleException(Exception exception) {
+        System.out.println(exception.getMessage() + "\n" + exception.getMessage() + "\n");
+        exception.printStackTrace();
+    }
+}
